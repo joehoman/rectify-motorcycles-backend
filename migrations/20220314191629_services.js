af533f96ff3a0d07944d3f00e8d162b9becb8fa9
Joseph Homan <jmhoman332@outlook.com>
2022-04-04 20:40:54 -0700

    seperated frontend and backend

---
+++ migrations/20220314191629_services.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    return knex.schema.createTable("services", (table) =>{
+        // id
+        table.increments("id");
+
+        // service
+        table.string("service").notNullable();
+
+        //cost
+        table.integer("cost");
+
+        //image
+        table.string("image"); 
+    });
+}
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("services");
+};
---
+++ seeds/01_users.js
@@ -0,0 +1,12 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+ exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('users').del()
+  await knex('users').insert([
+    {username: 'rowValue1', passwordHash: 'hashSlingingSlasher', first_name: 'Guy', last_name: 'Human', 
+    email: 'motoman@hotmail.com', address: '123 easy street, goodlife CO 80011', phone_number: '8002774233' }
+  ]);
+};
